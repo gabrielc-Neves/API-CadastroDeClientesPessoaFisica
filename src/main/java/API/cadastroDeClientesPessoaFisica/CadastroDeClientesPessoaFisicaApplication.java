2ce3ec28186ec60dea14a3533e73434a43b75e5c
gabrielc-Neves <gabrielcneves17@gmail.com>
2025-08-29 12:23:32 -0300

    Primeiro commit

---
+++ src/main/java/API/cadastroDeClientesPessoaFisica/CadastroDeClientesPessoaFisicaApplication.java
@@ -0,0 +1,13 @@
+package API.cadastroDeClientesPessoaFisica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeClientesPessoaFisicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeClientesPessoaFisicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API/cadastroDeClientesPessoaFisica/CadastroDeClientesPessoaFisicaApplicationTests.java
@@ -0,0 +1,13 @@
+package API.cadastroDeClientesPessoaFisica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeClientesPessoaFisicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
